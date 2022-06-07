11a18752f12e0aa1aa1d76cb128b3880f324621f
Peter Horsbøll Møller <peter.moller@pb.com>
2022-06-07 09:54:19 +0200

    v1.5.2
    v1.5.2 + v1.5.1:
    - Added support for numeric AREA_ID columns
    v1.5.0
    - New feature that allows the data loading from txt files into nativex tab files
    - Configuration dialog now supports the selection of boundary types
    - Save As now lets you add a project or area name to the resulting tab files.
    - Documentation

---
+++ codeMB/DynamicDemograpicAnalyzer.bat
@@ -0,0 +1,3 @@
+﻿@Echo off
+"C:\MapInfo\MapBasic\21.1\Mapbasic.Exe" -NOSPLASH -D "DDConfigDlg.mb" "DDDataLoading.mb" "DDLoadDataDlg.mb" "DynDemoAnalyzerMENU.mb" "DDFunctions.mb"
+IF not exist "*.err" echo compilation successful (v2021.0)
